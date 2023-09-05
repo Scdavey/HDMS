109411ae85e7339d86f2741f7b1e8c5757355019
Shane Davey <scdavey@lakeheadu.ca>
2023-09-05 19:26:53 -0400

    Home page styling
    Updated the styling to the home page for navigation.

--- home.php
+++ home.php
@@ -13,16 +13,20 @@ session_start();
 </head>
 
 <body>
-  <nav>
-    <a href="patients.php"><button>Patients</button></a>
-    <button>Staff</button>
-    <button>Appointments</button>
-    <button>Rooms</button>
-    <button>Availability</button>
-    <button>Emergencies</button>
-  </nav>
-
   <h1>Home</h1>
+
+  <nav class="nav-container">
+    <div class="column">
+      <a href="patients.php"><button>Patients</button></a>
+      <a href="staff.php"> <button>Staff</button></a>
+      <button>Appointments</button>
+    </div>
+    <div class="column">
+      <button>Rooms</button>
+      <button>Availability</button>
+      <button>Emergencies</button>
+    </div>
+  </nav>
 
   <?php if (isset($_SESSION["userid"])) : ?>
     <a class="logout" href="logout.php">Logout</a>

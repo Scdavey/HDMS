30266098043b810194deb2158534fa0f3686f5b2
Shane Davey <scdavey@lakeheadu.ca>
2023-09-11 19:12:04 -0400

    Edited nav drop down style
    Nav drop down is now wider and the transition speed is faster.

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidenav").style.height = "50px";
+  document.getElementById("mySidenav").style.height = "80px";
   document.getElementById("mySidenav").style.border = "1px solid black";
 }
 
